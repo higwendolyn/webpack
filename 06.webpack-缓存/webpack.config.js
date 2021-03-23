c159386de14956434b57f6bff964f297eebff4d2
Alias <2278660142@qq.com>
2021-03-23 22:39:31 +0800

    缓存完结

--- 06.webpack-缓存/webpack.config.js
+++ 06.webpack-缓存/webpack.config.js
@@ -2,6 +2,21 @@ const { resolve } = require('path');
 const HTMLWeboackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+/**
+ * 缓存:
+ *  babel缓存
+ *    cacheDirectory: true
+ *    ---> 让第二次打包构建速度更快
+ *  文件资源缓存
+ *    hash: 每次webpack构建时会生成一个唯一的hash值。
+ *      问题: 因为js和css同时使用一个hash值，如果重新打包，会导致所有缓存失效。（可能我却只改动一个文件）
+ *    chunkhash: 根据chunk生成的hash值。如果打包来源于同一个chunk，那么hash值就一样
+ *      问题: js和css的值还是一样的，因为css是在js中被引入的，所以同属于一个chunk
+ *    contenthash: 根据文件的内容生成hash值，不同的文件hash值一定不一样
+ *    ---> 让代码上线运行缓存更好使用
+ *  
+ */
+
 // 设置node环境变量
 process.env.NODE_ENV = 'production';
 
